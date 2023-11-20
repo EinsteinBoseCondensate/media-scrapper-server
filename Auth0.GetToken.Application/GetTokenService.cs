4a4e668de7175ca222b6ac2c8d14ef899068b735
EinsteinBoseCondensate <antoniom11190@gmail.com>
2023-11-20 19:10:24 +0100

    added try-catch

--- Auth0.GetToken.Application/GetTokenService.cs
+++ Auth0.GetToken.Application/GetTokenService.cs
@@ -1,7 +1,10 @@
-﻿using Auth0.AuthenticationApi;
+﻿using Amazon.Runtime.Internal.Util;
+using Auth0.AuthenticationApi;
 using Auth0.AuthenticationApi.Models;
 using Auth0.GetToken.Application.Config;
 using Auth0.GetToken.Application.Interfaces;
+using Common.Extensions;
+using Microsoft.Extensions.Logging;
 
 namespace Auth0.GetToken.Application;
 
@@ -10,11 +13,13 @@ public class GetTokenService : IGetTokenService
     private readonly IAuthenticationApiClient _authenticationApiClient;
     private static AccessTokenResponse? _accessTokenResponse;
     private static long? _accessTokenResponseRetrievalTicks;
+    private readonly ILogger<GetTokenService> _logger;
     public IAuth0M2MSettings Auth0M2MSettings { get; private set; }
-    public GetTokenService(IAuthenticationApiClient authenticationApiClient, IAuth0M2MSettings auth0M2MSettings)
+    public GetTokenService(IAuthenticationApiClient authenticationApiClient, IAuth0M2MSettings auth0M2MSettings, ILogger<GetTokenService> logger)
     {
         _authenticationApiClient = authenticationApiClient;
         Auth0M2MSettings = auth0M2MSettings;
+        _logger = logger;
     }
     public async Task<string> HandleAsync()
     {
@@ -23,12 +28,20 @@ public class GetTokenService : IGetTokenService
             return _accessTokenResponse!.AccessToken;
         }
 
-        _accessTokenResponse = await _authenticationApiClient.GetTokenAsync(new ClientCredentialsTokenRequest()
+        try
         {
-            Audience = Auth0M2MSettings.Audience,
-            ClientId = Auth0M2MSettings.ClientId,
-            ClientSecret = Auth0M2MSettings.ClientSecret
-        }).ConfigureAwait(false);
+            _accessTokenResponse = await _authenticationApiClient.GetTokenAsync(new ClientCredentialsTokenRequest()
+            {
+                Audience = Auth0M2MSettings.Audience,
+                ClientId = Auth0M2MSettings.ClientId,
+                ClientSecret = Auth0M2MSettings.ClientSecret
+            }).ConfigureAwait(false);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError($"Error grabbing accessTokenResponse, exception content was: {e.GetContent()}");
+            throw;
+        }
         _accessTokenResponseRetrievalTicks = DateTime.Now.Ticks;
         return _accessTokenResponse.AccessToken;
     }
@@ -36,6 +49,6 @@ public class GetTokenService : IGetTokenService
     private static bool IsCachedAccessTokenResponseValid()
     {
         return _accessTokenResponse != null && _accessTokenResponseRetrievalTicks != null &&
-            DateTime.Now.Ticks - _accessTokenResponseRetrievalTicks < _accessTokenResponse.ExpiresIn;
+            (DateTime.Now.Ticks - _accessTokenResponseRetrievalTicks)/10000 < _accessTokenResponse.ExpiresIn;
     }
 }
